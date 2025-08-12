085c8bc03c4f9ffda0f86876f1c857bd833e1108
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2025-08-12 14:17:44 +0000

    First Commit

--- FinanceManagement/Program.cs
+++ FinanceManagement/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FinanceManagement
+
+{
+    public record Transaction(int Id, DateTime Date, decimal Amount, string Category);
+
+    public static class Program
+{
+    public static void Main()
+    {
+        Transaction t1 = new Transaction(1, DateTime.Now, 150.75m, "Groceries");
+        Console.WriteLine($"Transaction: {t1.Id}, {t1.Date}, {t1.Amount}, {t1.Category}");
+        }
+    }
+}
